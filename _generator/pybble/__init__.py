0d75013fe7fdfa2e7f59e3486ff9a6bf9f391826
Wang Xuerui <idontknw.wang@gmail.com>
2015-04-30 21:23:51 +0800

    generator barebones

---
+++ _generator/pybble/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
---
+++ _generator/pybble/cli/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'entry',
+]
+
+
+def main(argv):
+    '''CLI Entrypoint.'''
+
+    print(argv)
+    return 0
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
