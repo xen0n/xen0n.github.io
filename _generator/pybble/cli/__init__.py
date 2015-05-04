29775e3a9b0f6e1d007e7e95cfad5d1515e308fc
Wang Xuerui <idontknw.wang@gmail.com>
2015-05-04 22:52:40 +0800

    pybble: use argparse

--- _generator/pybble/cli/__init__.py
+++ _generator/pybble/cli/__init__.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from .. import conf
 from .. import driver
 
+parser = argparse.ArgumentParser(
+        description='Pybble command-line interface.',
+        )
 
-def main(argv):
-    '''CLI Entrypoint.'''
+parser.add_argument('-c', dest='config', help='Config file to use')
 
-    # use default config for now
-    config = conf.load_config(None)
+subparsers = parser.add_subparsers(
+        title='sub-commands')
 
+parser_build = subparsers.add_parser(
+        'build',
+        help='Build the specified project',
+        )
+
+
+def build_cmd(config, args):
     # build stream driver
     stream_driver = driver.StreamDriver.from_config(
             config['streams'],
@@ -29,6 +40,22 @@ def main(argv):
     stream_driver.execute()
 
     return 0
+
+
+parser_build.set_defaults(func=build_cmd)
+
+
+def main():
+    '''CLI Entrypoint.'''
+
+    args = parser.parse_args()
+    config = conf.load_config(args.config)
+
+    if not hasattr(args, 'func'):
+        parser.print_usage()
+        return 1
+
+    return args.func(config, args)
 
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
